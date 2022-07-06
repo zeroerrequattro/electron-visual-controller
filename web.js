1cc80de846f7477d172bd577d080d67698bb317b
zeroerrequattro <daniele.biggiogero@gmail.com>
2022-07-07 00:53:13 +0200

    better autoclean that keeps last uploaded file

--- web.js
+++ web.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const formData = require('express-form-data')
+const fs = require('fs')
 const path = require('path')
-const os = require("os")
 const { webContents } = require('electron')
 
 const joinPath = string => path.join(`${__dirname}${string}`)
 const app = express()
 const port = 12627
 const router = express.Router()
+const tempDir = joinPath('/temp')
 const static = express.static(joinPath('/public'))
 const options = {
-  uploadDir: joinPath('/temp'),
+  uploadDir: tempDir,
   autoClean: false
 }
 
@@ -27,7 +28,18 @@ app.get('/', (req, res) => {
 
 app.post('/', formData.parse(options), (req, res) => {
   const { body, files } = req
+  const { name, path } = files.file
   const allWC = webContents.getAllWebContents()
+  const tempFiles = fs.readdirSync(tempDir)
+  const currentFile = path.replace(tempDir,'').substr(1)
+
+  tempFiles.forEach(tempFile => {
+    if (tempFile === currentFile) {
+      return
+    }
+
+    fs.unlinkSync(`${tempDir}/${tempFile}`)
+  })
 
   allWC.forEach(webContent => {
     webContent.send('tunnel:request', { body, files })
