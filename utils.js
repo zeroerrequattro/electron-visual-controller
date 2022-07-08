fdb25e263beae3b97f69bc4256fc36ab0c92ad1f
zeroerrequattro <daniele.biggiogero@gmail.com>
2022-07-09 01:38:24 +0200

    add crypto lib

--- utils.js
+++ utils.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
+const crypto = require('crypto')
 const path = require('path')
 
 const joinPath = string => path.join(`${__dirname}${string}`)
+
 const tempDir = joinPath('/temp')
+
 const cleanDir = excludeFilePath => {
   const tempFiles = fs.readdirSync(tempDir)
   const currentFile = excludeFilePath?.replace(tempDir,'').substr(1)
